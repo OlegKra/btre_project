501ae1dfc88368b2dcc067427aff796c7184a78c
Oleg <oleg-k@internet.ru>
2022-01-12 05:50:38 +0300

    added dynamic templating for listings and pages apps addede search functionality added pagination

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    ctx = {
+        "listings": listings,
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+    }
+    return render(request, 'pages/index.html', ctx)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.all().order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    ctx = {
+        "realtors": realtors,
+        "mvp_realtors": mvp_realtors,        
+    }
+    return render(request, 'pages/about.html', ctx)
